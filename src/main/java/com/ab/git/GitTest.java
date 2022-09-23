2a310f3c4df6ecb192b57da72aafb17c7118653d
loveab <114222058+loveab@users.noreply.github.com>
2022-09-23 20:34:14 +0800

    pull commit

--- src/main/java/com/ab/git/GitTest.java
+++ src/main/java/com/ab/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git22222!");
         System.out.println("hello git3!");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
