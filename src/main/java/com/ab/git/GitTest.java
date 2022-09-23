5d8afa0c17aac88b6b86e58012561143e7e4cc98
lgq <lgq@ab.com>
2022-09-23 20:06:23 +0800

    second submit

--- src/main/java/com/ab/git/GitTest.java
+++ src/main/java/com/ab/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ab.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git22222!");
     }
 }
